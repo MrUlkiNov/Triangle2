7062bcaef7cf95cbacc5190deb0ddc948e9ea150
MrUlkiNov <sam.baybekov@mail.ru>
2024-11-14 01:40:33 +0300

    Triangle2

---
+++ Triangle2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TriangleApp
+{
+    public enum TriangleType
+    {
+        Equilateral, 
+        Isosceles,   
+        Scalene,      
+        NotTriangle   
+    }
+
+    public class Triangle
+    {
+        public static TriangleType DetermineTriangleType(double a, double b, double c)
+        {
+            if (!IsValidTriangle(a, b, c))
+                return TriangleType.NotTriangle;
+
+            if (a == b && b == c)
+                return TriangleType.Equilateral;
+            else if (a == b || b == c || a == c)
+                return TriangleType.Isosceles;
+            else
+                return TriangleType.Scalene;
+        }
+
+        public static bool IsValidTriangle(double a, double b, double c)
+        {
+            return a > 0 && b > 0 && c > 0 &&
+                   a + b > c &&
+                   a + c > b &&
+                   b + c > a;
+        }
+
+        public static double CalculateArea(double a, double b, double c)
+        {
+            if (!IsValidTriangle(a, b, c))
+                throw new ArgumentException("The provided sides do not form a triangle.");
+
+            double semiPerimeter = (a + b + c) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            if (!Triangle.IsValidTriangle(a, b, c))
+            {
+                Console.WriteLine("Введенные значения не образуют треугольник.");
+            }
+            else
+            {
+                var type = Triangle.DetermineTriangleType(a, b, c);
+                Console.WriteLine($"Тип треугольника: {type}");
+
+                double area = Triangle.CalculateArea(a, b, c);
+                Console.WriteLine($"Площадь треугольника: {area}");
+            }
+        }
+    }
+}
---
+++ TriangleApp2.Tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using TriangleApp;
+
+namespace TriangleApp.Tests
+{
+    public class TriangleTests
+    {
+        [Theory]
+        [InlineData(3, 3, 3, TriangleType.Equilateral)]
+        [InlineData(5, 5, 8, TriangleType.Isosceles)]
+        [InlineData(3, 4, 5, TriangleType.Scalene)]
+        [InlineData(1, 2, 3, TriangleType.NotTriangle)]  
+        [InlineData(0, 4, 5, TriangleType.NotTriangle)]  
+        public void DetermineTriangleType_ReturnsCorrectType(double a, double b, double c, TriangleType expected)
+        {
+            var result = Triangle.DetermineTriangleType(a, b, c);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, true)]
+        [InlineData(1, 2, 3, false)]  
+        [InlineData(0, 4, 5, false)] 
+        [InlineData(-3, 4, 5, false)] 
+        public void IsValidTriangle_ReturnsCorrectValidation(double a, double b, double c, bool expected)
+        {
+            var result = Triangle.IsValidTriangle(a, b, c);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5, 6)]
+        [InlineData(5, 5, 8, 12)]
+        [InlineData(6, 6, 6, 15.588457268119896)]
+        public void CalculateArea_ReturnsCorrectArea(double a, double b, double c, double expected)
+        {
+            var result = Triangle.CalculateArea(a, b, c);
+            Assert.Equal(expected, result, 5); 
+        }
+
+        [Fact]
+        public void CalculateArea_WithInvalidTriangle_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => Triangle.CalculateArea(1, 2, 3));
+        }
+    }
+}
